bf3f53cdde3cc7e45f51b67ce1b65703098c1946
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-05 13:14:10 +0000

    data types and program documetation

---
+++ javascript-code/data-types.js
@@ -0,0 +1,16 @@
+//data types in js
+// primitive data types 
+//string
+let string="this is string"
+// console.log(typeof string)
+//number
+let number=2913
+// console.log(typeof number)
+let boolean=true;
+// console.log(typeof boolean)
+let undefined;
+// console.log(typeof undefined)
+let variable1=null
+// console.log(typeof variable1)
+let sym=Symbol("unique")
+console.log(typeof symbol)
---
+++ javascript-code/program-documentation.js
@@ -0,0 +1,9 @@
+//program documentation refer that good readability and modularity of code 
+//1.using unecessary indentaion and blank lines
+//2.avoid using semi colon it reduce readability of code
+console.log("good readability ")
+console.log(
+    "poor readability"
+
+)
+"use strict"; //it restrict the code will only use newer version of js
