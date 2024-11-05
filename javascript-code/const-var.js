b27dadaabf0f14cd5536201891a1a1599bf959c8
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-05 11:24:11 +0000

    const and var

--- javascript-code/const-var.js
+++ javascript-code/const-var.js
@@ -1,7 +1,9 @@
 const accountId="19087679"
-//this is a constant which doeasnot change if we re-initialize wiht new value
+//this is a constant which doeasnot change if we re-initialize wiht new value for example
+// accountId="786756"//thew error comment before run
 let accountName="codeloop"
 //let is variable which store some data
+accountName="pegasus"//codeloop are change into pegasus
 var password="nahi milega"
 //var are exception in nowdays due to scope do not use 
 city="delhi"
