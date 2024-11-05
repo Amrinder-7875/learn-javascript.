d6869e37f4a0d7c07cb84155fc47b159069837dd
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-05 11:18:06 +0000

    global-variable

---
+++ javascript-code/const-var.js
@@ -0,0 +1,9 @@
+const accountId="19087679"
+//this is a constant which doeasnot change if we re-initialize wiht new value
+let accountName="codeloop"
+//let is variable which store some data
+var password="nahi milega"
+//var are exception in nowdays due to scope do not use 
+city="delhi"
+//implicit variable
+console.table([accountId,accountName,password,city]);
