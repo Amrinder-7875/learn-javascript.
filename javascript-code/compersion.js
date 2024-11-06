d5980df7d58f0ab1ccad7874ab1b5d51e6511235
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-06 07:15:01 +0000

    compersion

---
+++ javascript-code/compersion.js
@@ -0,0 +1,25 @@
+//comparison with
+ null
+console.log("null comparsion")
+console.log(null>=0)
+console.log(null==0)
+console.log(null<=0)
+console.log(null>0)
+console.log(null>0)
+//comparison with undefined
+console.log("undefined comparsion")
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(undefined==0)
+//working algorithm of duble equal too are working deffrrently from >=,<= , therefore we get diffrent output then expected.
+//basic comparison
+console.log("basic comparsion")
+console.log(2>1)
+console.log(2<1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(2==1)
+//conversion when two diffrent data types 
+console.log("diffrent data type comparsion")
+console.log("2">0)//true because implicit type conversion
+console.log("2"<true) 
