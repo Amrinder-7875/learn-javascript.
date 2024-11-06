614a45e40a66df7bb976aa1202bcb0f0613e406b
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-06 02:57:20 +0000

    cnvertion of data types

---
+++ javascript-code/convertion-object.js
@@ -0,0 +1,13 @@
+//type convertion is used to convert datatype into another data type
+let convertion1="amrinder"
+let result=String(convertion1)//convert into string from number
+
+console.log(Number(convertion1));//print convertion into which data type
+console.log(typeof result);//type of convertion
+console.log( result);//value of variable 
+//more example
+//1.333->string=string
+//2."33"->number=number
+//3.true=1 & false=0
+//4."333abc"->number=number ->value=Nan
+//underfined=undefined
