c87854d285cf8d1d25f9b586a1d7036767d9d0ac
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-06 04:59:34 +0000

    operation

--- javascript-code/convertion-object.js
+++ javascript-code/convertion-object.js
@@ -2,12 +2,19 @@
 let convertion1="amrinder"
 let result=String(convertion1)//convert into string from number
 
-console.log(Number(convertion1));//print convertion into which data type
-console.log(typeof result);//type of convertion
-console.log( result);//value of variable 
+// console.log(Number(convertion1));//print convertion into which data type
+// console.log(typeof result);//type of convertion
+// console.log( result);//value of variable 
 //more example
 //1.333->string=string
 //2."33"->number=number
 //3.true=1 & false=0
 //4."333abc"->number=number ->value=Nan
 //underfined=undefined
+// *******************operation**************
+
+// arithmatic operation 
+console.log(2**10);//power
+console.log(2*2)//multiplication
+console.log(2+2)//addition
+console.log(2/2)//divition
