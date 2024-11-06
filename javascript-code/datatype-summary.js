16d6a4a1a51cbbe0d409b622bf3c29b123db1a55
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-06 09:35:21 +0000

    datatypes

---
+++ javascript-code/datatype-summary.js
@@ -0,0 +1,22 @@
+//primitive :- refrence was not given
+//string,boolean,number,undefined,null,symbol,BigInt
+const variable=false//boolean
+const num=29//number
+const string="amrinder "//string
+let undefined;//undefined
+let varoable=null;//null
+let log=Symbol('19283')//symbol
+let bignum=83883838827777777773927777777777777777777021n//bigint
+
+//non primitive :-refrence passed when call
+//array,object,funtion
+let arr=[12,32,43,23]
+console.log(arr)
+let obj={
+    name:"sldnf",
+    age: 238,
+}
+let examplefuntion=function(){
+    console.log(2>1);
+}
+console.log(obj)
