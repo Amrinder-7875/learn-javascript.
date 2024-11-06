252574a4f13b44a0371cf5b08ba23c998ba645ce
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-06 06:26:44 +0000

    precedence and associativity

--- javascript-code/convertion-object.js
+++ javascript-code/convertion-object.js
@@ -1,20 +1,46 @@
 //type convertion is used to convert datatype into another data type
-let convertion1="amrinder"
-let result=String(convertion1)//convert into string from number
+let convertion1=true
+let result=Boolean(convertion1)//convert into string from number
 
 // console.log(Number(convertion1));//print convertion into which data type
-// console.log(typeof result);//type of convertion
-// console.log( result);//value of variable 
+console.log(typeof result);//type of convertion
+console.log( result);//value of variable 
 //more example
 //1.333->string=string
 //2."33"->number=number
-//3.true=1 & false=0
+// console.log(2**10);//power
+// console.log(2*8)//multiplication
+// console.log(2+2)//addition
+// console.log(2/2)//divition
+// console.log(2%2)//molduler//3.true=1 & false=0
 //4."333abc"->number=number ->value=Nan
 //underfined=undefined
 // *******************operation**************
 
 // arithmatic operation 
-console.log(2**10);//power
-console.log(2*2)//multiplication
-console.log(2+2)//addition
-console.log(2/2)//divition
+
+
+// addition of two String
+let str1="amrinder"
+let str2=" singh"
+
+str3=str1+str2
+// console.log(str3)
+//*****************precedence**************************** 
+console.log(3 + 10 * 2); // 23
+console.log(3 + (10 * 2)); // 23, because parentheses here are superfluous
+console.log((3 + 10) * 2); // 26, because the parentheses change the order
+//precedence = high to low & associativity = left to right
+// 1.()
+//   **
+// 2.*,/,%
+// 3.++,--
+// 4.+,-
+// 5.=,+=,-=
+// ****************prefix and postfix incremnet************
+let prefix=10;
+console.log(prefix)
+++prefix;
+console.log(prefix)
+prefix++;
+console.log(prefix)
