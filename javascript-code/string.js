bb81934b69597c9ef81fb4a9d2e79cd021226b47
Amrinder-7875 <amrindersingh7522@gmail.com>
2024-11-08 05:29:14 +0000

    string as object

---
+++ javascript-code/string.js
@@ -0,0 +1,14 @@
+//method in js to write and declare a string 
+//baisc 
+console.log("world is hell")
+console.log('world is hell')
+let name="world is hell"
+console.log(name)
+//advanced
+//string into object
+let str=new String('hell the world')
+console.log(str)
+console.log(typeof str)
+//using the string as object is usefull when we have to acess the prototypes of string.
+//method to use the prototypes 
+console.log(str.length) 
